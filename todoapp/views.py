65abce25f4fbac37e5c5152f890ef24508c604bc
Coffeeplantation <118273075+Coffeeplantation@users.noreply.github.com>
2024-01-05 22:19:18 +0900

    models.py

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from . import views
 
 # Create your views here.
+def taskList(request):
+   return HttpResponse("<h1>Hello Django</h1>")
