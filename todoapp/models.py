c3cecd500cf81272f053a266afed8e0e9e6f870d
Coffeeplantation <118273075+Coffeeplantation@users.noreply.github.com>
2024-01-07 12:58:45 +0900

    migrate

--- todoapp/models.py
+++ todoapp/models.py
@@ -7,7 +7,7 @@ class Task(models.Model):
   title = models.CharField(max_length=100)
   description = models.TextField(null=True, blank=True)
   completed = models.BooleanField(default=False)
-  createdDate = models.DataTimeField(auto_now_add=True)
+  createdDate = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
     return self.title
