68e6b2da57bf8e76615e0c77b6999795498ac455
Coffeeplantation <118273075+Coffeeplantation@users.noreply.github.com>
2024-01-07 22:09:41 +0900

    todoリスト追加機能

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import TaskList, TaskDetail , TaskCreate
 
 urlpatterns = [
-path("", views.taskList)
+    path("", TaskList.as_view(), name="tasks"),
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
+    path("create-task/", TaskCreate.as_view(), name="create-task")
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,7 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from . import views
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy 
+
+from .models import Task
 
 # Create your views here.
-def taskList(request):
-   return HttpResponse("<h1>Hello Django</h1>")
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = "tasks"
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"  # or specify the fields you want to include
+    success_url = reverse_lazy("create-task")
