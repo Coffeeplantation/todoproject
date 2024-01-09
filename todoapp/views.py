d1950af5045f66b29218e0cbe7051c09e8224904
Coffeeplantation <118273075+Coffeeplantation@users.noreply.github.com>
2024-01-09 21:50:24 +0900

    ログインしているユーザーのみ、タスクを操作できるように修正

--- todoapp/views.py
+++ todoapp/views.py
@@ -7,29 +7,31 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from django.contrib.auth.views import LoginView
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Task
 
 # Create your views here.
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = "tasks"
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = "task"
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = "__all__"  # or specify the fields you want to include
     success_url = reverse_lazy("tasks")
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = "__all__"  # or specify the fields you want to include
     success_url = reverse_lazy("tasks")
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     fields = "__all__"  # or specify the fields you want to include
     success_url = reverse_lazy("tasks")
