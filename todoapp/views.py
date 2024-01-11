bdcc7614f5c471b6ebaa66399a0945cbed3aa200
Coffeeplantation <118273075+Coffeeplantation@users.noreply.github.com>
2024-01-11 22:09:49 +0900

    ユーザー新規登録、タスク検索機能実装

--- todoapp/views.py
+++ todoapp/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import render
 from django.urls import reverse_lazy 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 
 from django.contrib.auth.views import LoginView
-
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from .models import Task
 
@@ -17,14 +18,30 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = "tasks"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #context["programming"] = "python"
+        #return context
+        context["tasks"] = context["tasks"].filter(user=self.request.user)
+        searchInputText =self.request.GET.get("search")
+        if searchInputText:
+            context["tasks"] = context["tasks"].filter(title__icontains=searchInputText)
+
+        return context
+
+
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = "task"
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = "__all__"  # or specify the fields you want to include
+    fields = ["title", "description", "completed"]
     success_url = reverse_lazy("tasks")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
@@ -43,3 +60,14 @@ class TaskListLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy("tasks")
+
+class RegisterTodoApp(FormView):
+    template_name = "todoapp/register.html"
+    success_url = reverse_lazy("tasks")
+    form_class = UserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+            return super().form_valid(form)
