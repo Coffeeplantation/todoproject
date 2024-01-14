29ebb0b1de635faf50be50d09b87697776b3fbc4
Coffeeplantation <118273075+Coffeeplantation@users.noreply.github.com>
2024-01-14 21:59:10 +0900

    完了タスクに取り消し線を入れるまで

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -95,7 +95,7 @@ USE_TZ = True
 
 LOGIN_URL = "login"
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "todoapp" / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
